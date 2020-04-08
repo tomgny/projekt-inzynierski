423d1e48864d792fe4a46bff5b9ce61ba29ddd4a
tomgny <49343696+tomgny@users.noreply.github.com>
2020-04-08 12:57:51 +0200

    Add files via upload

---
+++ src/spring/proj/inz/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.tognyp.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(users.username("test").password("test123").roles("EMPLOYEE"))
+		.withUser(users.username("krystian").password("test123").roles("MANAGER"))
+		.withUser(users.username("tomek").password("test123").roles("ADMIN"));
+		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/showMyLoginPage")
+		.loginProcessingUrl("/authenticateTheUser")
+		.permitAll()
+		.and()
+		.logout()
+		.permitAll();
+	}
+	
+	
+	
+	
+
+}
